4448d4608b539715470eec380a9a767f2d366e97
ChanghaoQiu <2368831528@qq.com>
2024-12-02 16:26:56 +0800

    更新谐振IAE计算代码，当前版本可以实现每10个IAE计算一次平均IAE，使得IAE比较平滑，过零点的计算逻辑没变。

--- Matlab_Code/IAE_ZD_Test.m
+++ Matlab_Code/IAE_ZD_Test.m
@@ -1,7 +1,10 @@
 function [IAE, ZeroCount] = IAE_ZD(Err)
 
-persistent T IAE_Now IAE_Last Err_Last time zeroCrossCount counttime countstarttime
+%本函数实现了半个振动周期内IAE的运算，并统计一个滑动窗口内的过零点，如果过零点超出阈值可以判定谐振发生
 
+persistent T IAE_Now IAE_Last Err_Last time zeroCrossCount counttime countstarttime IAE_Sum IAE_Count
+
+%变量定义与初始化
 if isempty(T)
     T = 0.0001;
     IAE_Now = 0;
@@ -10,17 +13,27 @@ if isempty(T)
     time = 0;
     zeroCrossCount = 0;
     counttime = 100 * T;
-    countstarttime = 0.5;
+    countstarttime = 0.3;
+    IAE_Sum = 0; 
+    IAE_Count = 0;
 end
 
-if time >= 0.5
+%算法在0.3s后开始计算IAE，以避免初始时的突变，误差过小也没有必要计算IAE
+if (time >= 0.3)
     if sign(Err) == sign(Err_Last)
         IAE_Now = IAE_Now + abs(Err) * T;
     else
-    %检测出过零点,输出上一次的IAE，清零IAE
-        IAE_Last = IAE_Now;
+        IAE_Sum = IAE_Sum + IAE_Now;
         IAE_Now = 0;
         zeroCrossCount = zeroCrossCount + 1;
+        IAE_Count = IAE_Count + 1;
+
+        % 如果累计了10个误差积分值，计算平均值并输出
+        if IAE_Count == 10
+            IAE_Last = IAE_Sum / 10;
+            IAE_Sum = 0; 
+            IAE_Count = 0; 
+        end
     end
 end
 
@@ -33,10 +46,14 @@ end
 time = time + T;
 Err_Last = Err;
 
+%抑制后一般难以维持到很完美的误差为0，认为IAE抑制到小于1e-4就算完成
+if (IAE_Last < 1e-4)
+    IAE_Last = 0;
+    zeroCrossCount = 0;
+end
+
 IAE = IAE_Last;
 ZeroCount = zeroCrossCount;
 
 end
-
-
 
